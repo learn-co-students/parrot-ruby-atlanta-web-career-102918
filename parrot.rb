a26b186c703a4d6c5282fabad664fa41450a70fb
Scott D <j.s.dunn@icloud.com>
2018-10-25 15:07:05 -0400

    Done with Parrot lab.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,4 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(says="Squawk!")
+    puts "#{says}"
+    return says # Create method `parrot` that outputs a given phrase and
+end# returns the phrase
